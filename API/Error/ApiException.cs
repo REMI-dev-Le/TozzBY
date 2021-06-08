31b3466c9b01c1b7cfed306e04cb937e53636d36
Remichristopher <remichristopher28@gmail.com>
2021-06-08 10:53:36 +0530

    End of Section 7

---
+++ API/Error/ApiException.cs
@@ -0,0 +1,18 @@
+namespace API.Error
+{
+    public class ApiException
+    {
+        public ApiException(int statusCode, string message = null, string details = null)
+        {
+            StatusCode = statusCode;
+            this.message = message;
+            Details = details;
+        }
+
+        public int StatusCode { get; set; }
+
+        public string message { get; set; }
+
+        public string Details { get; set; }
+    }
+}
